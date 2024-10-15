cd378a23a2b744c7e5fc38f425e681bc1d48e42c
Juliana Girotto <ads.jjgirotto@gmail.com>
2024-10-15 16:20:36 -0300

    configs for file management, vo and exceptions

---
+++ src/main/java/br/com/erudio/rest_spring_java_erudio/exception/FileStorageException.java
@@ -0,0 +1,16 @@
+package br.com.erudio.rest_spring_java_erudio.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class FileStorageException extends RuntimeException {
+
+    public FileStorageException(String msg) {
+        super(msg);
+    }
+
+    public FileStorageException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
