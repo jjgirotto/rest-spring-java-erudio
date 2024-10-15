c942b7425f52abf2963ff15918e4d61d1b1f4477
Juliana Girotto <ads.jjgirotto@gmail.com>
2024-10-15 17:34:04 -0300

    upload multiple files and download methods

--- src/main/java/br/com/erudio/rest_spring_java_erudio/controller/FileController.java
+++ src/main/java/br/com/erudio/rest_spring_java_erudio/controller/FileController.java
@@ -3,15 +3,20 @@ package br.com.erudio.rest_spring_java_erudio.controller;
 import br.com.erudio.rest_spring_java_erudio.data.vo.v1.UploadFileResponseVO;
 import br.com.erudio.rest_spring_java_erudio.services.FileStorageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Tag(name = "File endpoint")
 @RestController
@@ -25,7 +30,7 @@ public class FileController {
 
     @PostMapping("/uploadFile")
     public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file) {
-        logger.info("Storing files into disk");
+        logger.info("Storing file into disk");
         var filename = service.storeFile(file);
         String fileDownloadUri = ServletUriComponentsBuilder
                 .fromCurrentContextPath()
@@ -35,4 +40,30 @@ public class FileController {
         return new UploadFileResponseVO(filename, fileDownloadUri,
                 file.getContentType(), file.getSize());
     }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        logger.info("Storing files into disk");
+        return Arrays.stream(files)
+                .map(file -> uploadFile(file))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/downloadFile/{filename:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename,
+                                                 HttpServletRequest request) {
+        logger.info("Reading file at disk");
+        Resource resource = service.loadFileAsResource(filename);
+        String contentType = "";
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (Exception e) {
+            logger.info("Could not determine file type");
+        }
+        if (contentType.isBlank()) contentType = "application/octet-stream";
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
 }
--- src/main/java/br/com/erudio/rest_spring_java_erudio/services/FileStorageService.java
+++ src/main/java/br/com/erudio/rest_spring_java_erudio/services/FileStorageService.java
@@ -2,7 +2,10 @@ package br.com.erudio.rest_spring_java_erudio.services;
 
 import br.com.erudio.rest_spring_java_erudio.config.FileStorageConfig;
 import br.com.erudio.rest_spring_java_erudio.exception.FileStorageException;
+import br.com.erudio.rest_spring_java_erudio.exception.MyFileNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -44,6 +47,17 @@ public class FileStorageService {
         } catch (Exception e) {
             throw new FileStorageException(
                     "Could not store file " + fileName + ". Please try again.", e);
+        }
+    }
+
+    public Resource loadFileAsResource(String filename) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(filename).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) return resource;
+            else throw new MyFileNotFoundException("File not found");
+        } catch (Exception e) {
+            throw new MyFileNotFoundException("File not found " + filename, e);
         }
     }
 }
